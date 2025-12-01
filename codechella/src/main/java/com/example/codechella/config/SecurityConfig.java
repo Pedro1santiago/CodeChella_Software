798b74fbd129f2b1d3e433696fc04d133792e8a1
Pedro1santiago <pedro.santigosiqueira@gmail.com>
2025-12-01 19:45:55 -0300

    Liberando autenticação aos user comuns

--- codechella/src/main/java/com/example/codechella/config/SecurityConfig.java
+++ codechella/src/main/java/com/example/codechella/config/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig {
                         .pathMatchers("/permissoes/solicitar").permitAll()
                         .pathMatchers("/permissoes/minhas-solicitacoes").permitAll()
                         .pathMatchers("/permissoes/pendentes").hasRole("SUPER")
-                        .pathMatchers("/permissoes/**/aprovar").hasRole("SUPER")
-                        .pathMatchers("/permissoes/**/negar").hasRole("SUPER")
+                        .pathMatchers("/permissoes/*/aprovar").hasRole("SUPER")
+                        .pathMatchers("/permissoes/*/negar").hasRole("SUPER")
                         .anyExchange().authenticated()
                 );
 
